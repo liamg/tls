63bad04ae20ddb5b0fddea01efbe9c0ba7f54a37
Liam Galvin <liam@liam-galvin.co.uk>
2020-04-19 18:09:39 +0100

    Tidying

--- pkg/tls/v1.2/client_hello_test.go
+++ pkg/tls/v1.2/client_hello_test.go
@@ -23,8 +23,10 @@ func TestClientHelloEncodingAndDecoding(t *testing.T) {
 		encoded          []byte
 	}{
 		{
-			sessionId: []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-				0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f},
+			sessionId: []byte{
+				0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+				0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+			},
 			supportedCiphers: []generic.CipherSuite{
 				generic.TLS_AES_128_GCM_SHA256,
 			},
@@ -46,8 +48,9 @@ func TestClientHelloEncodingAndDecoding(t *testing.T) {
 				0x49, 0x96, 0x2, 0xd2, // timestamp
 				0xd1, 0xd2, 0xd3, 0xd4, 0xd5, 0xd6, 0xd7, 0xd8, 0xd9, 0xda, 0xdb, 0xdc, 0xdd, 0xde, // random
 				0xe1, 0xe2, 0xe3, 0xe4, 0xe5, 0xe6, 0xe7, 0xe8, 0xe9, 0xea, 0xeb, 0xec, 0xed, 0xee, // ...
-				0x10,                                                                                           // session id length
-				0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, // session id
+				0x10,                                           // session id length
+				0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, // session id
+				0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, // ...
 				0x00, 0x02, //cipher suites length
 				0x13, 0x01, // aes 128 gcm sha256
 				0x01,       // compression method length
@@ -58,8 +61,7 @@ func TestClientHelloEncodingAndDecoding(t *testing.T) {
 				0x0, 0xd, // server names length
 				0x0,      // server name type: hostname
 				0x0, 0xa, // server name length: 10
-				0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x63, 0x6f, 0x6d, // extension data
-				// g     o     o     g     l     e     .     c     o     m
+				0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x63, 0x6f, 0x6d, // extension data: google.com
 			},
 		},
 	}
@@ -80,19 +82,19 @@ func TestClientHelloEncodingAndDecoding(t *testing.T) {
 			assert.Equal(t, test.encoded, data)
 		})
 
-		//t.Run(fmt.Sprintf("Decode #%d", i), func(t *testing.T) {
-		//
-		//	var hello ClientHello
-		//	err := hello.Decode(test.encoded)
-		//	require.NoError(t, err)
-		//
-		//	assert.Equal(t, generic.VersionTLS1_2, hello.Version)
-		//	assert.Equal(t, test.time, hello.Timestamp)
-		//	assert.Equal(t, test.random, hello.Random)
-		//	assert.Equal(t, test.sessionId, hello.SessionId)
-		//	assert.Equal(t, test.supportedCiphers, hello.SupportedCipherSuites)
-		//	assert.Equal(t, test.extensions, hello.Extensions)
-		//})
+		t.Run(fmt.Sprintf("Decode #%d", i), func(t *testing.T) {
+
+			var hello ClientHello
+			err := hello.Decode(test.encoded)
+			require.NoError(t, err)
+
+			assert.Equal(t, generic.VersionTLS1_2, hello.Version)
+			assert.Equal(t, test.time, hello.Timestamp)
+			assert.Equal(t, test.random, hello.Random)
+			assert.Equal(t, test.sessionId, hello.SessionId)
+			assert.Equal(t, test.supportedCiphers, hello.SupportedCipherSuites)
+			assert.Equal(t, test.extensions, hello.Extensions)
+		})
 	}
 
 }
